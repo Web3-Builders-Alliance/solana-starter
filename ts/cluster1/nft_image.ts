7642fe1d41220c4bf6090d7415b4ee96af3fba51
aresastro <dean@bitping.com>
2023-11-23 22:18:39 +0800

    Added NFT challenges

---
+++ ts/cluster1/nft_image.ts
@@ -0,0 +1,25 @@
+import wallet from "../wba-wallet.json"
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
+import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr"
+
+// Create a devnet connection
+const umi = createUmi('https://api.devnet.solana.com');
+const bundlrUploader = createBundlrUploader(umi);
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(signerIdentity(signer));
+
+(async () => {
+    try {
+        // const image = ???
+
+        // const [myUri] = ??? 
+        // console.log("Your image URI: ", myUri);
+    }
+    catch(error) {
+        console.log("Oops.. Something went wrong", error);
+    }
+})();
---
+++ ts/cluster1/nft_mint.ts
@@ -0,0 +1,26 @@
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
+import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+
+import wallet from "../wba-wallet.json"
+import base58 from "bs58";
+
+const RPC_ENDPOINT = "https://api.devnet.solana.com";
+const umi = createUmi(RPC_ENDPOINT);
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const myKeypairSigner = createSignerFromKeypair(umi, keypair);
+umi.use(signerIdentity(myKeypairSigner));
+umi.use(mplTokenMetadata())
+
+const mint = generateSigner(umi);
+
+(async () => {
+    // let tx = ???
+    // let result = await tx.sendAndConfirm(umi);
+    // const signature = base58.encode(result.signature);
+    
+    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+
+    console.log("Mint Address: ", mint.publicKey);
+})();
