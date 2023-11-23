67ae2aabefa6cf92f484007a18e88c5f418c642e
aresastro <dean@bitping.com>
2023-11-23 09:46:13 +0800

    Removed connection

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -4,13 +4,6 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createMetadataAccountV3 } from "@metaplex-foundation/mpl-token-metadata";
 import { createSignerFromKeypair, publicKey, signerIdentity, PublicKey as umiPublicKey } from "@metaplex-foundation/umi";
 
-// We're going to import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
-
-//Create a Solana devnet connection
-const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
-
 // Define our Mint address
 const mint = new PublicKey("<mint address>")
 
@@ -27,8 +20,8 @@ const [metadata_pda, _bump] = PublicKey.findProgramAddressSync(metadata_seeds, t
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
-const umiKeypair = umi.eddsa.createKeypairFromSecretKey(keypair.secretKey);
-umi.use(signerIdentity(createSignerFromKeypair(umi, umiKeypair)));
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
     try {
