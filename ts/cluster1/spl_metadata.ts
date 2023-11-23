d5814a05cad4e4f3507e180bbe506e996ee756be
aresastro <dean@bitping.com>
2023-11-23 10:33:34 +0800

    Fixed signer

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,11 +1,11 @@
-import { Commitment, Connection, Keypair, PublicKey, Transaction, sendAndConfirmTransaction } from "@solana/web3.js"
+import { PublicKey } from "@solana/web3.js"
 import wallet from "../wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createMetadataAccountV3 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, publicKey, signerIdentity, PublicKey as umiPublicKey } from "@metaplex-foundation/umi";
+import { createSignerFromKeypair, publicKey, signerIdentity, signerPayer, PublicKey as umiPublicKey } from "@metaplex-foundation/umi";
 
 // Define our Mint address
-const mint = new PublicKey("<mint address>")
+const mint = new PublicKey("GXWDj8NGR7GAH7Rm1i1MLCkC6NmZNh68FBKhPZJm5mQ1")
 
 // Add the Token Metadata Program
 const token_metadata_program_id = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s')
@@ -21,6 +21,7 @@ const [metadata_pda, _bump] = PublicKey.findProgramAddressSync(metadata_seeds, t
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
