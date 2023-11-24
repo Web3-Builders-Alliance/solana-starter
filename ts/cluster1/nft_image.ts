7d67817d15102eeca113470acc51a838267aa2ba
Dean 利迪恩 <10921578+deanmlittle@users.noreply.github.com>
2023-11-24 09:58:22 +0800

    Update nft_image.ts

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -2,7 +2,7 @@ import wallet from "../wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr"
-import { readFile } = "fs/promises"
+import { readFile } from "fs/promises"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
