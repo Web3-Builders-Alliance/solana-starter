28d5abaf0d26ac6286b4cd6f5b33f3c32b3c280a
Richard <ivmidable@gmail.com>
2024-02-22 07:28:36 -0500

    update to irys

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,20 +1,25 @@
 import wallet from "../wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr"
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
-const bundlrUploader = createBundlrUploader(umi);
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
+umi.use(irysUploader());
 umi.use(signerIdentity(signer));
+//1. Tell umi which uploader to use.
 
 (async () => {
     try {
+        //1. Load image
+        //2. Convert image to generic file.
+        //3. Upload image
+
         // const image = ???
 
         // const [myUri] = ??? 
