ab0e0812cd756a2fb624c89338a55dd253d57700
A91y <65825207+A91y@users.noreply.github.com>
2024-08-08 00:38:14 +0530

    feat: Mint new NFT with specified parameters

--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -16,11 +16,18 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    let tx = createNft(umi, {
+        mint,
+        name: "Ayu Rug",
+        symbol: "AYR",
+        uri: "https://arweave.net/0Yj6QO-_5Zpdj7ZGgIhGgckUEdOsXXFhTC3tMN-45BY",
+        sellerFeeBasisPoints: percentAmount(1),
+    })
+
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
