308892632dffcf487df64161e48296007a7aa536
A91y <65825207+A91y@users.noreply.github.com>
2024-08-08 00:38:24 +0530

    feat: Update nft_metadata.ts to include image URI and metadata for Ayu Rug

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -17,27 +17,31 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your image URI: ", myUri);
+        const image = "https://arweave.net/E1AkEkxMT8Eieo8anjZwIPVCXtd1gAoVdsaeaqzWv-Q";
+        const metadata = {
+            name: "Ayu Rug",
+            symbol: "AYR",
+            description: "Ayush's Rug",
+            image: image,
+            attributes: [
+                {trait_type: "hotness", value: "100"},
+                {trait_type: "coolness", value: "99"},
+                {trait_type: "awesomeness", value: "100"}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: [keypair.publicKey]
+        };
+        const myUri = await umi.uploader.uploadJson([metadata]);
+        console.log("Your image URI: ", myUri);
+        // https://arweave.net/a5NL0tkB1QrtLSqUgFesRDKX8ZIYPUkRif8wmJfTtX8
+        // https://arweave.net/0Yj6QO-_5Zpdj7ZGgIhGgckUEdOsXXFhTC3tMN-45BY
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
